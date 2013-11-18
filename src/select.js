c0169847e2893c501c712bd8e1c90c73a550bd0e
dimirc <wlacoka@gmail.com>
2013-11-17 23:57:37 -0500

    Separate templates on theme

--- src/select.js
+++ src/select.js
@@ -1,17 +1,18 @@
-angular.module('ui.select', ['ui.keypress']).directive('uiSelect', function($document,$timeout){
+angular.module('ui.select', ['ui.keypress'])
+
+.constant('uiSelectConfig', {
+  defaultTheme: 'select2',
+  defaultPlaceholder: 'Select Item',
+})
+
+.directive('uiSelect', function($document,$timeout,uiSelectConfig){
   return {
     restrict: 'E',
     /* jshint multistr: true */
-    template:
-    '<div class="ui-select-container" ng-class="{\'ui-select-container-active ui-select-dropdown-open\':open}"> \
-      <a class="ui-select-choice"></a> \
-      <div ng-class="{\'ui-select-display-none\':!open}" class="ui-select-drop ui-select-with-searchbox ui-select-drop-active"> \
-        <div class="ui-select-search"> \
-          <input class="ui-select-input" type="text" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" ui-keydown="{up: \'up()\', down: \'down()\', esc: \'close()\', enter: \'$select((data.items|filter: $select.search)[$select.index])\'}" ng-model="$select.search"> \
-        </div> \
-        <ul class="ui-select-results" /> \
-      </div> \
-    </div>',
+    templateUrl: function(tElement, tAttrs) {
+      var theme = tAttrs.theme || uiSelectConfig.defaultTheme;
+      return '../src/' + theme + '/select.tpl.html';
+    },
     replace: true,
     require: 'ngModel',
     transclude: true,
@@ -20,11 +21,6 @@ angular.module('ui.select', ['ui.keypress']).directive('uiSelect', function($doc
       return function($scope, $elm, $attrs, ngModel){
         transcludeFn($scope, function(clone) {
           $scope.open = false;
-          //Helper functions (no jQuery)
-          var hasClass = function (elem, klass) {
-            if (!elem) return;
-            return (" " + elem.className + " " ).indexOf( " "+klass+" " ) > -1;
-          };
           var getElementsByClassName = (function() {
             //To support IE8
             return document.getElementsByClassdName ?
@@ -38,13 +34,14 @@ angular.module('ui.select', ['ui.keypress']).directive('uiSelect', function($doc
 
           //Set transcluded elements to their correct position on template
           var transcluded = angular.element('<div/>').append(clone);
-          var transChoices = getElementsByClassName(transcluded[0],'ui-select-results');
-          var transMatch = getElementsByClassName(transcluded[0],'ui-select-choice');
-          getElementsByClassName($elm[0],'ui-select-results').replaceWith(transChoices[0]);
-          getElementsByClassName($elm[0],'ui-select-choice').replaceWith(transMatch[0]);
+          var transMatch = getElementsByClassName(transcluded[0],'ui-select-match');
+          var transChoices = getElementsByClassName(transcluded[0],'ui-select-choices');
+          getElementsByClassName($elm[0],'ui-select-match').replaceWith(transMatch);
+          getElementsByClassName($elm[0],'ui-select-choices').replaceWith(transChoices);
 
           var input = $elm.find('input');
-          $scope.activate = function(){
+          $scope.activate = function($event){
+            if ($event) $event.stopPropagation(); // Prevent bubbling
             $scope.open = true;
             //Give it time to appear before focus
             $timeout(function(){
@@ -79,45 +76,33 @@ angular.module('ui.select', ['ui.keypress']).directive('uiSelect', function($doc
             $scope.open = false;
             $scope.$select.search = "";
           };
-          var choiceArrow = getElementsByClassName(tElement[0],'ui-select-arrow');
-          var searchDiv = getElementsByClassName(tElement[0],'ui-select-search');
           var dismissClickHandler = function (evt) {
-            if (
-              hasClass(evt.target,'ui-select-choice') || //Chosen label for selection
-              hasClass(evt.target.parentElement,'ui-select-choice') || //No selection (Placeholder)
-              choiceArrow[0] === evt.target.parentElement ||
-              searchDiv[0] === evt.target.parentElement) {
+            if (angular.element(evt.target).hasClass('ui-select-search')){
               return;
             }
             $scope.close();
             $scope.$digest();
           };
           $document.bind('click', dismissClickHandler);
-          ngModel.$render = function(){
-            //$scope.$select.search = ngModel.$viewValue;
-          };
         });
       };
     }
   };
 })
 
-.directive('choices', function($sce) {
+.directive('choices', function($sce,uiSelectConfig) {
   return {
     // require: '^uiSelect',
     restrict: 'E',
     transclude: true,
     replace: true,
-    template:
-      '<ul class="ui-select-results">' +
-      '<li ng-class="{\'ui-select-highlighted\':$select.index==$index}"' +
-      ' ng-click="$select(item)"' +
-      ' ng-mousemove="$select.index=$index"' +
-      ' ng-repeat="empty">' +
-      ' <div class="ui-select-result-label" ng-transclude></div>' +
-      '</li></ul>',
+    templateUrl: function(tElement, tAttrs) {
+      //Gets theme atribute from parent (ui-select)
+      var theme = tElement[0].parentElement.getAttribute('theme') || uiSelectConfig.defaultTheme;
+      return '../src/' + theme + '/choices.tpl.html';
+    },
     compile: function(tElement, tAttrs, transcludeFn) {
-      tElement[0].children[0].setAttribute("ng-repeat", tAttrs.data);
+      tElement.find("li").attr("ng-repeat", tAttrs.data);
       return function(scope, element, attrs){
         scope.trustAsHtml = function(value) {
           return $sce.trustAsHtml(value);
@@ -138,23 +123,20 @@ angular.module('ui.select', ['ui.keypress']).directive('uiSelect', function($doc
   };
 })
 
-.directive('match', function($compile) {
+.directive('match', function($compile,uiSelectConfig) {
   return {
     restrict: 'E',
     transclude: true,
     replace: true,
-    /* jshint multistr: true */
-    template:
-      '<a href="javascript:void(0)" class="ui-select-choice" ng-click="activate()"> \
-        <span class="ui-select-arrow"><b></b></span> \
-        <span class="select2-chosen">{{selectedLabel}}</span> \
-      </a>',
+    templateUrl: function(tElement, tAttrs) {
+      //Gets theme atribute from parent (ui-select)
+      var theme = tElement[0].parentElement.getAttribute('theme') || uiSelectConfig.defaultTheme;
+      return '../src/' + theme + '/match.tpl.html';
+    },
     compile: function(tElement, tAttrs, transcludeFn) {
       return function($scope, $elm, $attrs, ngModel){
         transcludeFn($scope, function(clone) {
-          var labelWrap = angular.element("<div ng-hide='$select.selected'>" + tAttrs.placeholder + "</div>");
-          var labelWrapCompiled = $compile(labelWrap)($scope);
-          $elm.append(labelWrapCompiled);
+          $scope.placeholder = tAttrs.placeholder || uiSelectConfig.defaultPlaceholder;
           $elm.append(clone);
         });
       };
