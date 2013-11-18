917e520c25faf4c9a9a56ac69c98fe3932186dca
dimirc <wlacoka@gmail.com>
2013-11-18 02:13:11 -0500

    Create .ui-select-highlight to use with highlight filter

--- src/select.js
+++ src/select.js
@@ -149,6 +149,6 @@ angular.module('ui.select', ['ui.keypress'])
     return queryToEscape.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
   }
   return function(matchItem, query) {
-    return query ? matchItem.replace(new RegExp(escapeRegexp(query), 'gi'), '<strong>$&</strong>') : matchItem;
+    return query ? matchItem.replace(new RegExp(escapeRegexp(query), 'gi'), '<span class="ui-select-highlight">$&</span>') : matchItem;
   };
 });
