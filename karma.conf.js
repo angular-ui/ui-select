57255983257f13b81336353803ea2029618180f4
Tanguy Krotoff <tkrotoff@gmail.com>
2014-03-20 14:27:55 +0100

    jQuery layout changed between 1.10 and 1.11

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
 
     // List of files / patterns to load in the browser
     files: [
-      'bower_components/jquery/jquery.js',
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
 
