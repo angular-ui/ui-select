3fac88cfad0ad2369c567142eadba52bdb7998b1
Tanguy Krotoff <tkrotoff@gmail.com>
2014-04-01 14:58:45 +0200

    Update examples, add new filter propsFilter

--- examples/demo.js
+++ examples/demo.js
@@ -2,6 +2,43 @@
 
 var app = angular.module('demo', ['ngSanitize', 'ui.select']);
 
+/**
+ * AngularJS default filter with the following expression:
+ * "person in people | filter: {name: $select.search, age: $select.search}"
+ * performs a AND between 'name: $select.search' and 'age: $select.search'.
+ * We want to perform a OR.
+ */
+app.filter('propsFilter', function() {
+  return function(items, props) {
+    var out = [];
+
+    if (angular.isArray(items)) {
+      items.forEach(function(item) {
+        var itemMatches = false;
+
+        var keys = Object.keys(props);
+        for (var i = 0; i < keys.length; i++) {
+          var prop = keys[i];
+          var text = props[prop].toLowerCase();
+          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+            itemMatches = true;
+            break;
+          }
+        }
+
+        if (itemMatches) {
+          out.push(item);
+        }
+      });
+    } else {
+      // Let the output be the input untouched
+      out = items;
+    }
+
+    return out;
+  }
+});
+
 app.controller('DemoCtrl', function($scope, $http) {
   $scope.disabled = undefined;
 
@@ -32,13 +69,13 @@ app.controller('DemoCtrl', function($scope, $http) {
   ];
 
   $scope.address = {};
-  $scope.getAddress = function(address) {
+  $scope.refreshAddresses = function(address) {
     var params = {address: address, sensor: false};
     return $http.get(
       'http://maps.googleapis.com/maps/api/geocode/json',
       {params: params}
     ).then(function(response) {
-      return response.data.results;
+      $scope.addresses = response.data.results
     });
   };
 
