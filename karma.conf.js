fd4874bf6962c538155cd51b1e67cbc14eee0fc4
Wladimir Coka <wlacoka@gmail.com>
2014-07-07 13:54:21 -0500

    refactor(buid): change from grunt to gulp

--- karma.conf.js
+++ karma.conf.js
@@ -40,7 +40,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
