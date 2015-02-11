83b7eb16d05e7a8a0aac806793b810ac3fb602d6
James Homer <jameshomer85@gmail.com>
2015-02-11 17:49:39 +0000

    Allow sorting of multiple select

--- src/select.js
+++ src/select.js
@@ -90,6 +90,7 @@
   .constant('uiSelectConfig', {
     theme: 'bootstrap',
     searchEnabled: true,
+    sortEnabled: false,
     placeholder: '', // Empty by default, like HTML tag <select>
     refreshDelay: 1000, // In milliseconds
     closeOnSelect: true,
@@ -183,6 +184,7 @@
     ctrl.focusser = undefined; //Reference to input element used to handle focus events
     ctrl.disabled = undefined; // Initialized inside uiSelect directive link function
     ctrl.searchEnabled = undefined; // Initialized inside uiSelect directive link function
+    ctrl.sortEnabled = undefined; // Initialized inside uiSelect directive link function
     ctrl.resetSearchInput = undefined; // Initialized inside uiSelect directive link function
     ctrl.refreshDelay = undefined; // Initialized inside uiSelectChoices directive link function
     ctrl.multiple = false; // Initialized inside uiSelect directive link function
@@ -1078,6 +1080,11 @@
             $select.searchEnabled = searchEnabled !== undefined ? searchEnabled : uiSelectConfig.searchEnabled;
         });
 
+        scope.$watch('sortEnabled', function() {
+            var sortEnabled = scope.$eval(attrs.sortEnabled);
+            $select.sortEnabled = sortEnabled !== undefined ? sortEnabled : uiSelectConfig.sortEnabled;
+        });
+
         attrs.$observe('disabled', function() {
           // No need to use $eval() (thanks to ng-disabled) since we already get a boolean instead of a string
           $select.disabled = attrs.disabled !== undefined ? attrs.disabled : false;
@@ -1313,6 +1320,141 @@
           $select.sizeSearchInput();
         }
 
+      }
+    };
+  }])
+
+  // Make multiple matches sortable
+  .directive('uiSelectSort', ['$timeout', '$log', 'uiSelectConfig', 'uiSelectMinErr', function($timeout, $log, uiSelectConfig, uiSelectMinErr) {
+    return {
+      require: '^uiSelect',
+      link: function(scope, element, attrs, $select) {
+        if (scope[attrs.uiSelectSort] === null) {
+          throw uiSelectMinErr('sort', "Expected a list to sort");
+        }
+
+        var options = angular.extend({
+            axis: 'horizontal'
+          },
+          scope.$eval(attrs.uiSelectSortOptions));
+
+        var axis = options.axis,
+          draggingClassName = 'dragging',
+          droppingClassName = 'dropping',
+          droppingBeforeClassName = 'dropping-before',
+          droppingAfterClassName = 'dropping-after';
+
+        scope.$watch(function(){
+          return $select.sortEnabled;
+        }, function(n){
+          $log.log(n);
+          if (n) {
+            element.attr('draggable', true);
+          } else {
+            element.removeAttr('draggable');
+          }
+        });
+
+        element.on('dragstart', function(e) {
+          element.addClass(draggingClassName);
+
+          (e.dataTransfer || e.originalEvent.dataTransfer).setData('text/plain', scope.$index);
+        });
+
+        element.on('dragend', function() {
+          element.removeClass(draggingClassName);
+        });
+
+        var move = function(from, to) {
+          /*jshint validthis: true */
+          this.splice(to, 0, this.splice(from, 1)[0]);
+        };
+
+        var dragOverHandler = function(e) {
+          e.preventDefault();
+
+          var offset = axis === 'vertical' ? e.offsetY || e.layerY || (e.originalEvent ? e.originalEvent.offsetY : 0) : e.offsetX || e.layerX || (e.originalEvent ? e.originalEvent.offsetX : 0);
+
+          if (offset < (this[axis === 'vertical' ? 'offsetHeight' : 'offsetWidth'] / 2)) {
+            element.removeClass(droppingAfterClassName);
+            element.addClass(droppingBeforeClassName);
+
+          } else {
+            element.removeClass(droppingBeforeClassName);
+            element.addClass(droppingAfterClassName);
+          }
+        };
+
+        var dropTimeout;
+
+        var dropHandler = function(e) {
+          e.preventDefault();
+
+          var droppedItemIndex = parseInt((e.dataTransfer || e.originalEvent.dataTransfer).getData('text/plain'), 10);
+
+          // prevent event firing multiple times in firefox
+          $timeout.cancel(dropTimeout);
+          dropTimeout = $timeout(function() {
+            _dropHandler(droppedItemIndex);
+          }, 20);
+        };
+
+        var _dropHandler = function(droppedItemIndex) {
+          var theList = scope.$eval(attrs.uiSelectSort),
+            itemToMove = theList[droppedItemIndex],
+            newIndex = null;
+
+          if (element.hasClass(droppingBeforeClassName)) {
+            if (droppedItemIndex < scope.$index) {
+              newIndex = scope.$index - 1;
+            } else {
+              newIndex = scope.$index;
+            }
+          } else {
+            if (droppedItemIndex < scope.$index) {
+              newIndex = scope.$index;
+            } else {
+              newIndex = scope.$index + 1;
+            }
+          }
+
+          move.apply(theList, [droppedItemIndex, newIndex]);
+
+          scope.$apply(function() {
+            scope.$emit('uiSelectSort:change', {
+              array: theList,
+              item: itemToMove,
+              from: droppedItemIndex,
+              to: newIndex
+            });
+          });
+
+          element.removeClass(droppingClassName);
+          element.removeClass(droppingBeforeClassName);
+          element.removeClass(droppingAfterClassName);
+
+          element.off('drop', dropHandler);
+        };
+
+        element.on('dragenter', function() {
+          if (element.hasClass(draggingClassName)) {
+            return;
+          }
+
+          element.addClass(droppingClassName);
+
+          element.on('dragover', dragOverHandler);
+          element.on('drop', dropHandler);
+        });
+
+        element.on('dragleave', function() {
+          element.removeClass(droppingClassName);
+          element.removeClass(droppingBeforeClassName);
+          element.removeClass(droppingAfterClassName);
+
+          element.off('dragover', dragOverHandler);
+          element.off('drop', dropHandler);
+        });
       }
     };
   }])
