3a8c50a589a74c6a801a35b03ba6745af889b199
Yadong Wen <yadongwen@users.noreply.github.com>
2015-07-30 03:33:03 +0800

    avoid errors when $select.selected is null

--- src/uiSelectMultipleDirective.js
+++ src/uiSelectMultipleDirective.js
@@ -56,7 +56,7 @@ uis.directive('uiSelectMultiple', ['uiSelectMinErr','$timeout', function(uiSelec
 
       ctrl.getPlaceholder = function(){
         //Refactor single?
-        if($select.selected.length) return;
+        if($select.selected && $select.selected.length) return;
         return $select.placeholder;
       };
 
