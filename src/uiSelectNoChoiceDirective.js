c944307778dd78f18bf19e9287d532f57c7ce733
Bogdan Alexe <bogdan.alexe@adswizz.com>
2016-05-31 14:48:06 +0300

    fix(uiSelectNoChoice): make compatible with Angular 1.5
    ...and non-cached templates. 
    
    Same change introduced in 0e85670ed377148ac3a7d5ef6ff5ed7c46446858
    
    Closes #1609

--- src/uiSelectNoChoiceDirective.js
+++ src/uiSelectNoChoiceDirective.js
@@ -6,6 +6,9 @@ uis.directive('uiSelectNoChoice',
             replace: true,
             transclude: true,
             templateUrl: function (tElement) {
+                // Needed so the uiSelect can detect the transcluded content
+                tElement.addClass('ui-select-no-choice');
+      
                 // Gets theme attribute from parent (ui-select)
                 var theme = tElement.parent().attr('theme') || uiSelectConfig.theme;
                 return theme + '/no-choice.tpl.html';
