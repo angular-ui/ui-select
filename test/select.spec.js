92110abf16c5dc855b61c6623ca2fd4e19caad7f
Jefiozie <jefiozie.bosch@gmail.com>
2017-04-15 16:24:58 +0200

    Removed test code of the header and footer.

--- test/select.spec.js
+++ test/select.spec.js
@@ -55,21 +55,15 @@ describe('ui-select tests', function () {
       link: function (scope, element, attrs, ngModel) {
         ngModel.$validators.testValidator = function (modelValue, viewValue) {
           if (isNil(modelValue)) {
-            link: function link(scope, element, attrs, ngModel) {
-              ngModel.$validators.testValidator = function (modelValue, viewValue) {
-                if (angular.isUndefined(modelValue) || modelValue === null) {
-                  return true;
-                } else if (angular.isArray(modelValue)) {
-                  var allValid = true, idx = modelValue.length;
-                  while (idx-- > 0 && allValid) {
-                    allValid = allValid && modelValue[idx].valid;
-                  }
-                  return allValid;
-                } else {
-                  return !!modelValue.valid;
-                }
-              };
+            return true;
+          } else if (angular.isArray(modelValue)) {
+            var allValid = true, idx = modelValue.length;
+            while (idx-- > 0 && allValid) {
+              allValid = allValid && modelValue[idx].valid;
             }
+            return allValid;
+          } else {
+            return !!modelValue.valid;
           }
         }
       }
@@ -3382,54 +3376,6 @@ describe('ui-select tests', function () {
       triggerKeydown(searchInput, Key.Down);
       triggerKeydown(searchInput, Key.Enter);
       expect(el.scope().$select.activeIndex).toBe(2);
-    });
-  });
-
-  describe('Header & Footer', function () {
-
-    var el;
-
-    function setupSelectComponent(theme, isMulti) {
-      isMulti = !!isMulti;
-      var multi = isMulti ? ' multiple' : '';
-      var selectedStr = isMulti ? '$select.selected[0]' : '$select.selected';
-      scope.selection.selected = isMulti ? [scope.people[0]] : scope.people[0];
-
-      el = compileTemplate(
-        '<ui-select ng-model="selection.selected" theme="' + theme + '"' + multi + '> \
-          <ui-select-match placeholder="Pick one...">{{$select.selected.name}}</ui-select-match> \
-          <ui-select-header>{{' + selectedStr + '.name}}</ui-select-header>\
-          <ui-select-choices repeat="person in people | filter: $select.search"> \
-            <div ng-bind-html="person.name | highlight: $select.search"></div> \
-            <div ng-bind-html="person.email | highlight: $select.search"></div> \
-          </ui-select-choices> \
-          <ui-select-footer>{{' + selectedStr + '.name}}</ui-select-footer> \
-        </ui-select>'
-      );
-    }
-
-    ['selectize', 'bootstrap', 'select2'].forEach(function (theme) {
-      describe(theme + ' theme', function () {
-        it('should show the header', function () {
-          setupSelectComponent(theme);
-          expect($(el).find('.ui-select-header').text().trim()).toBe(scope.selection.selected.name);
-        });
-
-        it('should show the header in multiple option', function () {
-          setupSelectComponent(theme, true);
-          expect($(el).find('.ui-select-header').text().trim()).toBe(scope.selection.selected[0].name);
-        });
-
-        it('should show the footer', function () {
-          setupSelectComponent(theme);
-          expect($(el).find('.ui-select-footer').text().trim()).toBe(scope.selection.selected.name);
-        });
-
-        it('should show the footer in multiple option', function () {
-          setupSelectComponent(theme, true);
-          expect($(el).find('.ui-select-footer').text().trim()).toBe(scope.selection.selected[0].name);
-        });
-      });
     });
   });
 });
